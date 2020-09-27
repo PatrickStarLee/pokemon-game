c9afa0594af141f8f6204dcc7eeeaa54769a5b73
Patrick Lee <patricklee260@gmail.com>
2020-09-27 16:19:27 -0700

    Delete testing.py

--- testing.py
+++
@@ -1,9 +0,0 @@
-class Animal:
-    def __init__(self, voice):
-        self.voice = voice
-
-    def sound(self):
-        return 'I heard a {}'.format(self.voice)
-
-cat = Animal('Meow')
-print(cat.sound())
